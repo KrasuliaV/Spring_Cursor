293ab298216b068a5a8fa92c63782673a887cf9f
NadyaYar <nadya.yarmak03@gmail.com>
2022-08-12 23:14:19 +0300

    javaConfig

--- src/main/java/config/Config.java
+++ src/main/java/config/Config.java
@@ -1,4 +1,4 @@
-package Config;
+package config;
 
 import javaConfig.Calculator;
 import javaConfig.MinusService;
--- src/main/java/javaConfig/Application.java
+++ src/main/java/javaConfig/Application.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 public class Application {
     public static void main(String[] args) {
         ApplicationContext applicationContext
-                = new AnnotationConfigApplicationContext("Config");
+                = new AnnotationConfigApplicationContext("config");
 
         Calculator calculator = applicationContext.getBean(Calculator.class);
 
